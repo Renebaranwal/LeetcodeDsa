5e59ca164c070b63a010f3235ebd04a67c8dfb8a
Rene baranwal <121424157+Renebaranwal@users.noreply.github.com>
2026-02-01 16:08:25 +0530

    Add files via upload

---
+++ leetcode485.java
@@ -0,0 +1,25 @@
+public class leetcode485 {
+    public static int maxcons(int[] nums)
+    {
+        int n = nums.length;
+        int count = 0;
+        int max_count = 0;
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == 1)
+            {
+                count++;
+                max_count = Math.max(max_count, count);
+            } else {
+                count = 0;
+            }
+        }
+        return max_count;
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,1,0,1,1,1};
+        System.out.println(maxcons(nums));
+    }
+    
+}
